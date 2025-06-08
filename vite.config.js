1c786ff3d4240b0905cbb7ba8d08c38e92e27684
Koosell <nasalzamaizzar29@gmail.com>
2025-06-08 10:51:06 +0700

    Update Terbaru

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import compression from 'vite-plugin-compression'
-
-export default defineConfig({
-  base: '/Final_Project/', // GANTI sesuai nama repo kamu
-  plugins: [react(), compression()],
-})
