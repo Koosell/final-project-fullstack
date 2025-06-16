29eecf2cbfbc1c273164abbe2e3891fc8347841d
Koosell <nasalzamaizzar29@gmail.com>
2025-06-16 20:58:03 +0700

    Tambah Panel Admin

--- final-project-frontend/vite.config.js
+++ final-project-frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import compression from 'vite-plugin-compression'
 
 export default defineConfig({
-  base: '/final-project-fullstack/', // GANTI sesuai nama repo kamu
   plugins: [react(), compression()],
+  // base: '/',   // Tidak perlu di-set, biarkan default
 })
