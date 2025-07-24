6746c3ecf8ca94d97139024c0a63b5c952c3dc45
Koosell <nasalzamaizzar29@gmail.com>
2025-07-24 22:59:40 +0700

    fix: set base path for vite config

--- final-project-frontend/vite.config.js
+++ final-project-frontend/vite.config.js
@@ -4,5 +4,5 @@ import compression from 'vite-plugin-compression'
 
 export default defineConfig({
   plugins: [react(), compression()],
-  base: '/final-project-fullstack/', // <-- KEMBALIKAN KE PENGATURAN INI
+  base: '/', // <-- KEMBALIKAN KE PENGATURAN INI
 })
